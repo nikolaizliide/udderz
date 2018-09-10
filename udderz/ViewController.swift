9b6a219ff0b88d82148b5ca0c7590d6447621851
nikolai lindholm <nikolaibrix1@hotmail.com>
2018-09-10 10:57:10 +0200

    Initial Commit

---
+++ udderz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  udderz
+//
+//  Created by nikolai lindholm on 10/09/2018.
+//  Copyright © 2018 nikolai lindholm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
